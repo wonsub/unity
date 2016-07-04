2e5f811dc8f078fa78643852e05ed24fd9e3bc93
wonsub <secretfice@naver.com>
2016-07-04 23:00:52 +0900

    유니티 예제 1
    공굴리기

---
+++ Roll a ball/Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	
+	}
+	
+	
+    //update 처리 이후에 처리됨
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	
+	}
+}
